1d447a0e5f2f65df753f5b17b304dceb7abd7989
Sergio <sergioa.orozcoo@gmail.com>
2022-07-27 23:14:33 -0500

    Change Text Value after toggle

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let handleCheck = document.getElementById('switchTheme')
 handleCheck.addEventListener('change', function () {
   let bodyElement = document.querySelector('body')
+  let colorMode = document.getElementById('textValue')
   if (this.checked) {
-    // colorMode = 'Light Color'
+    colorMode.innerHTML = 'Light Color'
     bodyElement.classList.remove('isLightColor')
     bodyElement.classList.add('isDarkColor')
   } else {
+    colorMode.innerHTML = 'Dark Color'
     bodyElement.classList.remove('isDarkColor')
     bodyElement.classList.add('isLightColor')
     // colorMode = 'Dark Color'
